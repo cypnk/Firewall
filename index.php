c724e6bac81989de1de876d180ee81b78b280e75
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-07-14 06:43:03 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Skip local IP address validation (for testing or running on Tor)
+// (true = localhost)
+define( 'SKIP_LOCAL',		true );
+
+// Get firewall
+require( 'firewall.php' );
+
+// Remember to set the database setting in firewall.php
+
+// Rest of your code...
+
